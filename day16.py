f067c07f0f582c451abace82f9fda3f4e8f20016
akanksha singh <akankshasinghmarko123@gmail.com>
2025-07-01 19:41:26 +0530

    Add files via upload

---
+++ day16.py
@@ -0,0 +1,14 @@
+# a =[1,2,43]
+# # b= [1,2,43]
+# a=3
+# b=3
+# print(a is b)#exact location of obje t in memory
+# print(a==b)#value
+# print(a is 3)
+
+
+# oops in python
+RailwayForm
+harry
+tom
+subham
