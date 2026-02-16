550c17221d8089ed37907de9b64d0cead02b8c01
akanksha singh <akankshasinghmarko123@gmail.com>
2026-02-16 21:37:29 +0530

    Fake News Generators

--- main.py
+++ main.py
@@ -1,4 +1,43 @@
-# PythonFinalizationError  
-print("hello world")
-print(6)
-# this is my first python program
+import random 
+# create subject
+
+subject=[
+    "shahrukh khan",
+    'virat kohli',
+    'modi ji',
+    'Rahul gandhi',
+    'a group of Monkey',
+    'Shrama Ji ka ldka'
+]
+
+action =[
+    'Lauches',
+    "Cancel sports",
+    "dancing",
+    "orders breakfast",
+    "celebrate"
+]
+places_or_things =[
+    "at Red Fort",
+    "in Mumbai Local Train",
+    "during Ipl",
+    " in shahdol",
+    "at Gandhi Chouk"
+]
+
+# start the head line generation loop
+
+while True:
+    subject = random.choice(subject)
+    action= random.choice(action)
+    places_or_things=random.choice(places_or_things)
+
+    headline =f"Breaking News:{subject} {action} {places_or_things}"
+    print("\n "+headline)
+    # ask user 
+
+    user_input =input("/n Do you want another headline ? (yes/no)").strip().lower()
+    if user_input =="no":
+     break
+# print good by 
+print("\n "+"Thanks for using Fake News Headline Gnerator have a fun day")
